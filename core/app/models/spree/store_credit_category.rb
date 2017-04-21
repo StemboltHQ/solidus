fb18a0d88dd1c2d49bde4b9ad31866eb6837c533
Luuk Veenis <luuk@stembolt.com>
2017-04-21 13:31:49 -0700

    Make store credits non-expiring by default
    This is essentially how store credits behave currently. We have an
    "expiring" type, but without an expiration they don't really expire.
    
    If we continue to make expiring the default, we have to worry about what
    to set the default expiration date to, since there's now a validation
    that requires it for expiring credit types.

--- core/app/models/spree/store_credit_category.rb
+++ core/app/models/spree/store_credit_category.rb
@@ -1,6 +1,6 @@
 class Spree::StoreCreditCategory < Spree::Base
-  class_attribute :non_expiring_credit_types
-  self.non_expiring_credit_types = [Spree.t("store_credit.non_expiring")]
+  class_attribute :non_expiring_credit_categories
+  self.non_expiring_credit_categories = [Spree.t("store_credit_category.default")]
 
   class_attribute :reimbursement_category_name
   self.reimbursement_category_name = Spree.t("store_credit_category.default")
@@ -11,6 +11,6 @@ class Spree::StoreCreditCategory < Spree::Base
   end
 
   def non_expiring?
-    self.class.non_expiring_credit_types.include? name
+    self.class.non_expiring_credit_categories.include? name
   end
 end
